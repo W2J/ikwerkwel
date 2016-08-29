19221acbfd206c84827cdf937c5b48ce24d400a3
Edwin Stehouwer <Estehouwer@werkwel.nl>
2016-08-29 15:10:36 +0200

    Toevoeging hamburger modal

---
+++ W2J_012/scripts/W2J_vul_modal.js
@@ -0,0 +1,28 @@
+var menuPaginas = ["Samen Werken aan een ideëel doel", "Wat WerkWel doet", "Wat de vrijwilligers doen", "Wat is WerkWel", "Contact", "Intranet" ];
+
+
+function vulModal()  {
+  var ul = document.getElementById("modal-content-id");	
+  for (var i = 0; i < menuPaginas.length; i++)  {
+    var li = document.getElementById("li");
+    if (li) {
+      li.parentNode.removeChild(li);
+    };
+    var li = document.createElement("li");
+    var ahref = document.createElement("a");
+    var lineBreak = document.createElement("br");
+    var pageTitle = (i + 1) + " - " + menuPaginas[i];
+    ahref.appendChild(document.createTextNode(pageTitle));
+    ahref.appendChild(lineBreak);
+    ahref.setAttribute("href", "#");
+    ahref.setAttribute("id", "ahrefId" + (i+1));
+    ahref.setAttribute("class", "ahrefClass");
+    var openPagNr = "openPage('d" + (i + 1) + "\')"
+    ahref.setAttribute("onclick", openPagNr);
+    ahref.classList.add("listClass");
+    li.appendChild(ahref);
+    li.setAttribute("id", "hbMenu" + (i+1));
+    li.setAttribute("class", "hbClass");
+    ul.appendChild(li);
+  }
+}  
